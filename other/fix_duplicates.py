065f1d2be8837817cf76d5002bb15fbca6ddfd78
ArchontPnz <emzot96@gmail.com>
2019-12-30 14:10:13 +0300

    add array_filling.py

---
+++ other/fix_duplicates.py
@@ -0,0 +1,13 @@
+def fix_duplicates(array):
+    res = []
+    for i, v in enumerate(array):
+        c = array[:i].count(v)
+        if c:
+            res.append(v + '_' + str(c))
+        else:
+            res.append(v)
+    return res
+
+
+chars = ["a", "b", "c", "a", "a", "d", "c"]
+print(fix_duplicates(chars))
